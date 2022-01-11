246197160b49dd93392a2643fd6bb28a51dc0338
MarkChrisLevy <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>
2022-01-11 10:54:45 +0100

    initial commit

--- dist/_esm2015/firestore/buildQuery.js
+++ dist/_esm2015/firestore/buildQuery.js
@@ -39,34 +39,36 @@ export function buildQuery(query, ...queryConstraints) {
     }
     else {
         if (queryConstraints) {
+            let niu = query;
             for (const constraint of queryConstraints) {
                 const type = constraint[0];
                 const args = constraint.slice(1);
                 if (type === "where") {
-                    query.where.call(query, ...args);
+                    niu = niu.where.call(niu, ...args);
                 }
                 else if (type === "limit") {
-                    query.limit.call(query, ...args);
+                    niu.limit.call(niu, ...args);
                 }
                 else if (type === "endBefore") {
-                    query.endBefore.call(query, ...args);
+                    niu.endBefore.call(niu, ...args);
                 }
                 else if (type === "limitToLast") {
-                    query.limitToLast.call(query, ...args);
+                    niu.limitToLast.call(niu, ...args);
                 }
                 else if (type === "orderBy") {
-                    query.orderBy.call(query, ...args);
+                    niu.orderBy.call(niu, ...args);
                 }
                 else if (type === "startAfter") {
-                    query.startAfter.call(query, ...args);
+                    niu.startAfter.call(niu, ...args);
                 }
                 else if (type === "startAt") {
-                    query.startAt.call(query, ...args);
+                    niu.startAt.call(niu, ...args);
                 }
                 else if (type === "endAt") {
-                    query.endAt.call(query, ...args);
+                    niu.endAt.call(niu, ...args);
                 }
             }
+            return niu;
         }
         return query;
     }
