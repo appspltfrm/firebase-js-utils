cd6cb195d92a76d2414cdd912d2d7d237a5b4094
MarkChrisLevy <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>
2022-09-09 10:53:27 +0200

    commit

--- dist/_esm2015/firestore/serverTimestamp.js
+++ dist/_esm2015/firestore/serverTimestamp.js
@@ -2,7 +2,7 @@ import { serverTimestamp as serverTimestampClient } from "firebase/firestore";
 import { Firestore } from "./Firestore";
 export function serverTimestamp() {
     if (Firestore.adminInitialized()) {
-        Firestore.admin().FieldValue.serverTimestamp();
+        return Firestore.admin().FieldValue.serverTimestamp();
     }
     else {
         return serverTimestampClient();
--- dist/firestore/serverTimestamp.js
+++ dist/firestore/serverTimestamp.js
@@ -5,7 +5,7 @@ const firestore_1 = require("firebase/firestore");
 const Firestore_1 = require("./Firestore");
 function serverTimestamp() {
     if (Firestore_1.Firestore.adminInitialized()) {
-        Firestore_1.Firestore.admin().FieldValue.serverTimestamp();
+        return Firestore_1.Firestore.admin().FieldValue.serverTimestamp();
     }
     else {
         return (0, firestore_1.serverTimestamp)();
